ed9b326d35bcd17fa19c200ff1464ef47a339d91
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-07-26 15:23:35 +0530

    created page one screen onboarding

--- lib/features/onboarding/widgets/page_one.dart
+++ lib/features/onboarding/widgets/page_one.dart
@@ -1,5 +1,9 @@
 import 'package:advanced_flutter_todo_app/common/utils/constants.dart';
+import 'package:advanced_flutter_todo_app/common/widgets/appstyle.dart';
+import 'package:advanced_flutter_todo_app/common/widgets/height_spacer.dart';
+import 'package:advanced_flutter_todo_app/common/widgets/reuseable_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PageOne extends StatelessWidget {
   const PageOne({super.key});
@@ -10,6 +14,43 @@ class PageOne extends StatelessWidget {
       height: AppConst.kHeight,
       width: AppConst.kWidth,
       color: AppConst.kBKDark,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.w),
+            child: Image.asset('assets/images/todo.png'),
+          ),
+          const HeightSpacer(height: 100),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ReuseableText(
+                text: 'ToDo with Shani',
+                style: appstyle(
+                  30,
+                  AppConst.kLight,
+                  FontWeight.w600,
+                ),
+              ),
+              const HeightSpacer(height: 10),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.w),
+                child: Text(
+                  'Welcome! Do you want to create task fast and ease?',
+                  textAlign: TextAlign.center,
+                  style: appstyle(
+                    16,
+                    AppConst.kGreyLight,
+                    FontWeight.normal,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
