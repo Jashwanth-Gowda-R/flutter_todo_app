8097dffb9697b2ae1ec60c5931faf8c15a0b15e0
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-07-31 20:36:08 +0530

    Detached TabBar and Tab bar view

--- lib/features/todo/pages/homepage.dart
+++ lib/features/todo/pages/homepage.dart
@@ -3,6 +3,7 @@ import 'package:advanced_flutter_todo_app/common/widgets/appstyle.dart';
 import 'package:advanced_flutter_todo_app/common/widgets/custom_text.dart';
 import 'package:advanced_flutter_todo_app/common/widgets/height_spacer.dart';
 import 'package:advanced_flutter_todo_app/common/widgets/reuseable_text.dart';
+import 'package:advanced_flutter_todo_app/common/widgets/width_spacer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
@@ -15,8 +16,15 @@ class HomePage extends ConsumerStatefulWidget {
   ConsumerState<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends ConsumerState<HomePage> {
+class _HomePageState extends ConsumerState<HomePage>
+    with TickerProviderStateMixin {
   final TextEditingController _search = TextEditingController();
+
+  late final TabController tabController = TabController(
+    length: 2,
+    vsync: this,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,13 +92,115 @@ class _HomePageState extends ConsumerState<HomePage> {
           ),
         ),
       ),
-      body: Center(
-        child: ReuseableText(
-          text: 'Shani Todo',
-          style: appstyle(
-            26,
-            Colors.blue,
-            FontWeight.bold,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: ListView(
+            children: [
+              const HeightSpacer(height: 25),
+              Row(
+                children: [
+                  const Icon(
+                    FontAwesome.tasks,
+                    size: 20,
+                    color: AppConst.kLight,
+                  ),
+                  const WidthSpacer(width: 10),
+                  ReuseableText(
+                    text: 'Today\'s Task',
+                    style: appstyle(
+                      18,
+                      AppConst.kLight,
+                      FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const HeightSpacer(height: 25),
+              Container(
+                decoration: BoxDecoration(
+                  color: AppConst.kLight,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(AppConst.kRadius),
+                  ),
+                ),
+                child: TabBar(
+                  controller: tabController,
+                  indicator: BoxDecoration(
+                    color: AppConst.kGreyLight,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(AppConst.kRadius),
+                    ),
+                  ),
+                  indicatorSize: TabBarIndicatorSize.label,
+                  labelPadding: EdgeInsets.zero,
+                  isScrollable: false,
+                  labelColor: AppConst.kBlueLight,
+                  labelStyle: appstyle(
+                    24,
+                    AppConst.kBlueLight,
+                    FontWeight.w700,
+                  ),
+                  unselectedLabelColor: AppConst.kLight,
+                  tabs: [
+                    Tab(
+                      child: SizedBox(
+                        width: AppConst.kWidth * 0.5,
+                        child: Center(
+                          child: ReuseableText(
+                            text: 'Pending',
+                            style: appstyle(
+                              16,
+                              AppConst.kBKDark,
+                              FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Tab(
+                      child: Container(
+                        padding: EdgeInsets.only(left: 30.w),
+                        width: AppConst.kWidth * 0.5,
+                        child: Center(
+                          child: ReuseableText(
+                            text: 'Completed',
+                            style: appstyle(
+                              16,
+                              AppConst.kBKDark,
+                              FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const HeightSpacer(height: 20),
+              SizedBox(
+                height: AppConst.kHeight * 0.3,
+                width: AppConst.kWidth,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(AppConst.kRadius),
+                  ),
+                  child: TabBarView(
+                    controller: tabController,
+                    children: [
+                      Container(
+                        height: AppConst.kHeight * 0.3,
+                        color: AppConst.kBkLight,
+                      ),
+                      Container(
+                        height: AppConst.kHeight * 0.3,
+                        color: AppConst.kBkLight,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
