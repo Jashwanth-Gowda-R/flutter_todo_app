ea52bb57fe57979fb0388bc18f866796cc36c8b7
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-08-02 22:24:53 +0530

    Setting up datetime picker and consuming our providers

--- lib/features/todo/pages/add_todo.dart
+++ lib/features/todo/pages/add_todo.dart
@@ -3,9 +3,12 @@ import 'package:advanced_flutter_todo_app/common/widgets/appstyle.dart';
 import 'package:advanced_flutter_todo_app/common/widgets/custom_otn_btn.dart';
 import 'package:advanced_flutter_todo_app/common/widgets/custom_text.dart';
 import 'package:advanced_flutter_todo_app/common/widgets/height_spacer.dart';
+import 'package:advanced_flutter_todo_app/features/todo/controllers/dates/dates_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart'
+    as picker;
 
 class AddTask extends ConsumerStatefulWidget {
   const AddTask({super.key});
@@ -20,6 +23,10 @@ class _AddTaskState extends ConsumerState<AddTask> {
 
   @override
   Widget build(BuildContext context) {
+    var scheduleDate = ref.watch(dateStateProvider);
+    var startTime = ref.watch(startTimeStateProvider);
+    var endTime = ref.watch(finishTimeStateProvider);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -53,10 +60,33 @@ class _AddTaskState extends ConsumerState<AddTask> {
             CustomOtlnBtn(
               height: 52.h,
               width: AppConst.kWidth,
-              text: 'Add Date',
+              text: scheduleDate == ''
+                  ? 'Add Date'
+                  : scheduleDate.substring(0, 10),
               color: AppConst.kLight,
               color2: AppConst.kBlueLight,
-              onTap: () {},
+              onTap: () {
+                picker.DatePicker.showDatePicker(
+                  context,
+                  showTitleActions: true,
+                  minTime: DateTime(2023, 8, 1),
+                  maxTime: DateTime(2080, 8, 1),
+                  theme: const picker.DatePickerTheme(
+                    doneStyle: TextStyle(
+                      color: AppConst.kGreen,
+                      fontSize: 16,
+                    ),
+                  ),
+                  onConfirm: (date) {
+                    print('confirm $date');
+                    ref
+                        .read(dateStateProvider.notifier)
+                        .setDate(date.toString());
+                  },
+                  currentTime: DateTime.now(),
+                  locale: picker.LocaleType.en,
+                );
+              },
             ),
             const HeightSpacer(height: 20),
             Row(
@@ -65,19 +95,64 @@ class _AddTaskState extends ConsumerState<AddTask> {
                 CustomOtlnBtn(
                   height: 52.h,
                   width: AppConst.kWidth * 0.4,
-                  text: 'Start Time',
+                  text: startTime == ''
+                      ? 'Start Time'
+                      : startTime.toString().substring(11, 16),
                   color: AppConst.kLight,
                   color2: AppConst.kBlueLight,
-                  onTap: () {},
+                  onTap: () {
+                    picker.DatePicker.showDateTimePicker(context,
+                        showTitleActions: true,
+                        // minTime: DateTime(2020, 5, 5, 20, 50),
+                        // maxTime: DateTime(2020, 6, 7, 05, 09),
+                        onConfirm: (date) {
+                      print('confirm $date');
+                      ref
+                          .read(startTimeStateProvider.notifier)
+                          .setStart(date.toString());
+                    }, locale: picker.LocaleType.en);
+                  },
                 ),
-                CustomOtlnBtn(
-                  height: 52.h,
-                  width: AppConst.kWidth * 0.4,
-                  text: 'End Time',
-                  color: AppConst.kLight,
-                  color2: AppConst.kBlueLight,
-                  onTap: () {},
-                ),
+                startTime == ''
+                    ? CustomOtlnBtn(
+                        height: 52.h,
+                        width: AppConst.kWidth * 0.4,
+                        text: endTime == ''
+                            ? 'End Time'
+                            : endTime.toString().substring(11, 16),
+                        color: AppConst.kLight,
+                        color2: AppConst.kGreyDk,
+                        onTap: () {
+                          // picker.DatePicker.showDateTimePicker(context,
+                          //     showTitleActions: true,
+                          //     minTime: end, onConfirm: (date) {
+                          //   print('confirm $date');
+                          //   ref
+                          //       .read(finishTimeStateProvider.notifier)
+                          //       .setEnd(date.toString());
+                          // }, locale: picker.LocaleType.en);
+                        },
+                      )
+                    : CustomOtlnBtn(
+                        height: 52.h,
+                        width: AppConst.kWidth * 0.4,
+                        text: endTime == ''
+                            ? 'End Time'
+                            : endTime.toString().substring(11, 16),
+                        color: AppConst.kLight,
+                        color2: AppConst.kBlueLight,
+                        onTap: () {
+                          picker.DatePicker.showDateTimePicker(context,
+                              showTitleActions: true,
+                              minTime: DateTime.parse(startTime),
+                              onConfirm: (date) {
+                            print('confirm $date');
+                            ref
+                                .read(finishTimeStateProvider.notifier)
+                                .setEnd(date.toString());
+                          }, locale: picker.LocaleType.en);
+                        },
+                      ),
               ],
             ),
             const HeightSpacer(height: 20),
