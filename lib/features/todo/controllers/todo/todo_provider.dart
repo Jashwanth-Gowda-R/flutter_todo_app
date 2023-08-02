8a5153e653e6430412284c69d1e708ae4f7253c4
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-08-02 16:42:47 +0530

    Generating todo provider and todo related logic

---
+++ lib/features/todo/controllers/todo/todo_provider.dart
@@ -0,0 +1,79 @@
+import 'package:advanced_flutter_todo_app/common/helpers/db_helper.dart';
+import 'package:advanced_flutter_todo_app/common/models/task_models.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'todo_provider.g.dart';
+
+@riverpod
+class TodoState extends _$TodoState {
+  @override
+  List<TaskModel> build() {
+    return [];
+  }
+
+  void refresh() async {
+    final data = await DBHelper.getItems();
+    state = data.map((e) => TaskModel.fromJson(e)).toList();
+  }
+
+  void addItem(TaskModel task) async {
+    final data = await DBHelper.createItem(task);
+    refresh();
+  }
+
+  void updateItem(int id, String title, String desc, String date,
+      String startTime, String endTime, int isCompleted) async {
+    final data = await DBHelper.updateItem(
+        id, title, desc, date, startTime, endTime, isCompleted);
+    refresh();
+  }
+
+  Future<void> deleteTodo(int id) async {
+    final data = await DBHelper.deleteItem(id);
+    refresh();
+  }
+
+  void markAsCompleted(int id, String title, String desc, String date,
+      String startTime, String endTime, int isCompleted) async {
+    final data =
+        await DBHelper.updateItem(id, title, desc, date, startTime, endTime, 1);
+    refresh();
+  }
+
+  String getToday() {
+    DateTime today = DateTime.now();
+    return today.toString().substring(0, 10);
+  }
+
+  String getTommorow() {
+    DateTime tommorow = DateTime.now().add(
+      const Duration(days: 1),
+    );
+    return tommorow.toString().substring(0, 10);
+  }
+
+  List<String> getLast30Days() {
+    DateTime today = DateTime.now();
+    DateTime oneMonthAgo = today.subtract(
+      const Duration(days: 30),
+    );
+    List<String> dates = [];
+    for (var i = 0; i < 30; i++) {
+      DateTime date = oneMonthAgo.add(
+        Duration(days: i),
+      );
+      dates.add(date.toString().substring(0, 10));
+    }
+    return dates;
+  }
+
+  bool getStatus(TaskModel data) {
+    bool? isCompleted;
+
+    if (data.isCompleted == 0) {
+      isCompleted = false;
+    } else {
+      isCompleted = true;
+    }
+    return isCompleted;
+  }
+}
---
+++ lib/features/todo/controllers/todo/todo_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$todoStateHash() => r'a0a7ceb47a160ea8df5124856351d634988aecc0';
+
+/// See also [TodoState].
+@ProviderFor(TodoState)
+final todoStateProvider =
+    AutoDisposeNotifierProvider<TodoState, List<TaskModel>>.internal(
+  TodoState.new,
+  name: r'todoStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todoStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TodoState = AutoDisposeNotifier<List<TaskModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
