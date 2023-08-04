15634a279c29fd516ac23154a566bb495878b98a
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-08-05 00:52:50 +0530

    add firebase to project n setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:advanced_flutter_todo_app/common/utils/constants.dart';
 import 'package:advanced_flutter_todo_app/features/todo/pages/homepage.dart';
+import 'package:advanced_flutter_todo_app/firebase_options.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+// com.googleusercontent.apps.108755459227-kv3ffi68a88j06vd16daeu832vbmc5t5
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
